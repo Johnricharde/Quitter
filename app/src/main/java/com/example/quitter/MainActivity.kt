9b0bc2f5f9a7ad2f739fe5baa8504fe32f447650
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2024-05-14 10:28:33 +0200

    continued working on styling and placeholders

--- app/src/main/java/com/example/quitter/MainActivity.kt
+++ app/src/main/java/com/example/quitter/MainActivity.kt
@@ -4,16 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.quitter.ui.theme.QuitterTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,6 +42,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
 fun MainPage(modifier: Modifier = Modifier) {
     Column(
@@ -40,23 +51,76 @@ fun MainPage(modifier: Modifier = Modifier) {
             .fillMaxSize()
     )
     {
-        Column(modifier = modifier)
-        {
-            Text(text = "You've gone")
-            Text(text = "31 days")
-            Text(text = "without smoking!")
+        // Header //////////////////////////////////////////////////////////////////////////
+        Surface(
+            color = colorResource(id = R.color.gray),
+            modifier = modifier
+                .padding(top = 16.dp)
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            )
+            {
+                Text(text = "You've gone", color = colorResource(id = R.color.white))
+                Text(text = "31 days", color = colorResource(id = R.color.white))
+                Text(text = "without smoking!", color = colorResource(id = R.color.white))
+            }
         }
-        Column {
-            Text(text = "QUITTER")
+        // Body ////////////////////////////////////////////////////////////////////////////
+        Surface(
+            modifier = modifier
+                .padding(top = 16.dp)
+                .fillMaxWidth()
+                .weight(2f)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    text = "QUITTER",
+                    color = colorResource(id = R.color.gray),
+                    fontSize = 48.sp,
+                    fontWeight = FontWeight.Bold,
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.quitter_logo),
+                    contentDescription = "Quitter Logo"
+                )
+            }
         }
-        Column {
-            Text(text = "I have a craving!")
-            Text(text = "Achievements")
-            Text(text = "Statistics")
-            Text(text = "Settings")
+        // Footer //////////////////////////////////////////////////////////////////////////
+        Surface(
+            color = colorResource(id = R.color.gray),
+            modifier = modifier
+                .padding(top = 16.dp)
+                .fillMaxWidth()
+                .weight(2f)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Button(
+                    onClick = { /* Do something! */ },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = colorResource(id = R.color.white),
+                        contentColor = colorResource(id = R.color.gray)
+                    )
+                ) {
+                    Text("I have a craving!")
+                }
+                Text(text = "Achievements", color = colorResource(id = R.color.white))
+                Text(text = "Statistics", color = colorResource(id = R.color.white))
+                Text(text = "Settings", color = colorResource(id = R.color.white))
+            }
         }
     }
 }
+
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
