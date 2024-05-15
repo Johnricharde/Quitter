22e1240019feb39f9d3417cea8132366797f0379
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2024-05-15 13:40:50 +0200

    added settingsPage and statisticsPage view with some static placeholders and styling

---
+++ app/src/main/java/com/example/quitter/StatisticsPage.kt
@@ -0,0 +1,60 @@
+package com.example.quitter
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.quitter.ui.theme.QuitterTheme
+
+@Composable
+fun StatisticsPage(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxSize()
+    ) {
+        Surface(
+            shape = RoundedCornerShape(20.dp),
+            modifier = modifier
+                .padding(top = 16.dp)
+                .fillMaxWidth()
+                .weight(4f)
+        ) {
+
+        }
+        Surface(
+            color = colorResource(id = R.color.gray),
+            shape = RoundedCornerShape(20.dp),
+            modifier = modifier
+                .padding(top = 16.dp)
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.SpaceEvenly,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                // Go back button
+                FilledButton(onClick = { /*TODO*/ }, text = "Go back")
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun StatisticsPagePagePreview() {
+    QuitterTheme {
+        StatisticsPage()
+    }
+}
